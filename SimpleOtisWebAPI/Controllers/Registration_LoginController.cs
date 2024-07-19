c4973a0f1314356c65a94350f6c50615688afb27
Manjunath D C <manjunathdc1997@gmail.com>
2024-07-19 22:38:32 +0530

    Added the JWT Authentication in Login and added the Authorize attribute to all the controllers except for the Register_LoginController and configured an attribute for isRedis to fetch the bool to return between In-memeory and distributed cache.

--- SimpleOtisWebAPI/Controllers/Registration_LoginController.cs
+++ SimpleOtisWebAPI/Controllers/Registration_LoginController.cs
@@ -2,10 +2,14 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.IdentityModel.Tokens;
 using SimpleOtisAPI.Domain.DTOs;
 using SimpleOtisAPI.Domain.Interfaces;
 using SimpleOtisWebAPI.Models;
 using SimpleOtisWebAPI.Services;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace SimpleOtisWebAPI.Controllers
 {
@@ -14,9 +18,11 @@ namespace SimpleOtisWebAPI.Controllers
     public class Registration_LoginController : ControllerBase
     {
         private readonly IRegister_Login _register_login;
-        public Registration_LoginController(IRegister_Login register_login)
+        private readonly IConfiguration _configuration;
+        public Registration_LoginController(IRegister_Login register_login, IConfiguration configuration)
         {
             _register_login = register_login;
+            _configuration = configuration;
         }
         [HttpPost("register")]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -33,6 +39,7 @@ namespace SimpleOtisWebAPI.Controllers
                 Register_LoginDBDTO user = new()
                 {
                     User_Name = register.User_Name,
+                    Azure_ID = Guid.NewGuid().ToString(),
                     PasswordHash = PasswordHash,
                     PasswordSalt = PasswordSalt,
                 };
@@ -61,16 +68,46 @@ namespace SimpleOtisWebAPI.Controllers
         {
             try
             {
-                Register_LoginDBDTO user = new Register_LoginDBDTO
+                Register_LoginDBDTO loginUser = new Register_LoginDBDTO
                 {
                     User_Name = login.User_Name
                 };
 
-                Register_LoginDBDTO result = await _register_login.Login(user);
+                Register_LoginDBDTO user = await _register_login.Login(loginUser);
 
-                bool isLoggedIn = PasswordHasher.VerifyPasswordHasher(login.Password, result.PasswordHash, result.PasswordSalt);
+                bool isLoggedIn = PasswordHasher.VerifyPasswordHasher(login.Password, user.PasswordHash, user.PasswordSalt);
 
-                return isLoggedIn ? StatusCode(StatusCodes.Status200OK, $"Login is Successful") : StatusCode(StatusCodes.Status500InternalServerError, $"Login is not successfull, Please check the User_Name or Password");
+                var returnResult = new Object();
+
+                if(isLoggedIn && user!=null)
+                {
+                    var claims = new[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                        new Claim(JwtRegisteredClaimNames.Iss, _configuration["Jwt:Issuer"]),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim("UserName", user.User_Name),
+                        new Claim("AzureID", user.Azure_ID),
+                    };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                    var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+                    var token = new JwtSecurityToken(
+                            _configuration["Jwt:Issuer"],
+                            _configuration["Jwt:Audience"],
+                            claims,
+                            expires: DateTime.UtcNow.AddMinutes(60),
+                            signingCredentials: signIn
+                      );
+
+                    var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+
+                    returnResult = Ok(new {Token = tokenValue, user});
+                }
+
+                return (IActionResult)returnResult;
+
+                //return isLoggedIn ? StatusCode(StatusCodes.Status200OK, $"Login is Successful") : StatusCode(StatusCodes.Status500InternalServerError, $"Login is not successfull, Please check the User_Name or Password");
             }
             catch (Exception ex)
             {
